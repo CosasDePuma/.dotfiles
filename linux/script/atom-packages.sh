892400c335cc2534413340ea0fa4d5e0a73d3976
MiniPuma <kikefontanlorenzo@gmail.com>
2018-12-27 01:00:53 +0100

    :rocket: One more package
    - Atom-Live-Server

--- linux/script/atom-packages.sh
+++ linux/script/atom-packages.sh
@@ -6,7 +6,7 @@
 
 dependencies=( dialog )
 available_plugins=(
-  ask-stack autoclose-html autocomplete-paths
+  atom-live-server ask-stack autoclose-html autocomplete-paths
   auto-update-packages emmet expose file-icons highlight-selected
   language-markdown markdown-pdf minimap pigments platformio-ide-terminal
   project-manager )
@@ -101,6 +101,7 @@ show_dialog() {
   Press SPACE to mark/unmark an option.
   "                                                                             \
     20 70 10                                                                    \
+    Atom-Live-Server        "Launch a http server with reloads" on              \
     Ask-Stack               "Quickly get answers from Stack Overflow" off       \
     Autoclose-HTML          "Automates closing of HTML Tags" off                \
     Autocomplete-Paths      "Adds path autocompletion to autocomplete+" on      \
